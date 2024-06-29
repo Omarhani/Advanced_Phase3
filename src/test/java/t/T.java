c95ad3034225f3a4d39fc29aa29fe0326ac09682
Omar Hany <OmarMohamed20150362@fci.helwan.edu.eg>
2024-06-29 14:38:45 +0300

    Session 1

---
+++ src/test/java/t/T.java
@@ -0,0 +1,31 @@
+package t;
+
+import base.BaseTests;
+import org.testng.annotations.Test;
+
+import java.io.FileNotFoundException;
+
+public class T extends BaseTests {
+
+    @Test
+    public void t1() throws FileNotFoundException {
+//        readDataFromJson = new ReadDataFromJson();
+//        System.out.println(readDataFromJson.readJsonFile().URL);
+        System.out.println(dataModel().URL);
+    }
+    @Test
+    public void t2() throws FileNotFoundException {
+        System.out.println(dataModel().Login.ValidCredentials.Username);
+        System.out.println(dataModel().Login.ValidCredentials.Password);
+    }
+    @Test
+    public void t3() throws FileNotFoundException {
+        System.out.println(dataModel().Login.InvalidCredentials.InvalidUsername.Username);
+        System.out.println(dataModel().Login.InvalidCredentials.InvalidUsername.Password);
+    }
+    @Test
+    public void t4() throws FileNotFoundException {
+        System.out.println(dataModel().Login.InvalidCredentials.InvalidPassword.Username);
+        System.out.println(dataModel().Login.InvalidCredentials.InvalidPassword.Password);
+    }
+}
