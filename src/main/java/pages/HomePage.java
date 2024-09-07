efe2710c635ef4d2835ebcbd3f0de5121944ca9a
eslam646 <eslamnasser796@gmail.com>
2024-09-07 20:46:53 +0300

    test contact us form

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -14,6 +14,7 @@ public class HomePage extends MethodHandles {
     private final By homePageLink = By.xpath("//a[normalize-space()='Home']");
     private final By loggedLabel = By.xpath("//a[contains(normalize-space(),'Logged in as')]");
     private final By deleteAccountLink=By.linkText("Delete Account");
+    private final By contactUsLink = By.xpath("//a[contains(text(),'Contact us')]");
 
     //Actions
     public boolean homePageVisible() {
@@ -40,6 +41,11 @@ public class HomePage extends MethodHandles {
     {
         click(logoutLink,3);
         return new LoginPage(driver);
+    }
+
+    public ContactUsPage clickOnContactUsLink(){
+        click(contactUsLink,3);
+        return new ContactUsPage(driver);
     }
 
 
