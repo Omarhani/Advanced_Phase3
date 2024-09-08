b15edbe8664aa4d23c07a6a89765bdc2247b6416
MfawzyIbrahim <mahmoud.f.s.ibrahim@gmail.com>
2024-09-08 22:19:10 +0300

    Register User with existing email

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -14,14 +14,21 @@ public class LoginPage extends MethodHandles {
     private final By loginText= By.xpath("//h2[normalize-space()='Login to your account']");
     private final By emailField=By.name("email");
     private final By passwordField=By.name("password");
+    private final By signupEmailField=By.xpath("//input[@data-qa='signup-email']");
     private final By loginButton=By.xpath("//button[normalize-space()='Login']");
     private final By validationMessage =By.xpath("//p[contains(text(),'Your email or password is incorrect!')]");
-
-
+    private final By newUserText=By.xpath("//h2[contains(text(),'New User Signup!')]");
+    private final By signupButton=By.xpath("//button[normalize-space()='Signup']");
+    private final By nameField=By.xpath("//input[@placeholder='Name']");
+    private final By getMessageEmailExist=By.xpath("//p[normalize-space()='Email Address already exist!']");
     //Actions
         public boolean loginTextVisible ()
     {
         return  isDisplayed(loginText,3);
+    }
+    public boolean newUserTextVisible ()
+    {
+        return  isDisplayed(newUserText,3);
     }
     private void insertEmail(String text)
     {
@@ -31,14 +38,26 @@ public class LoginPage extends MethodHandles {
     {
         sendKeys(passwordField,3,text);
     }
+    private void insertName(String text)
+    {
+        sendKeys(nameField,3,text);
+    }
     private void clickOnLoginButton()
     {
         click(loginButton,3);
     }
-
-
-
-
+    private void clickOnSignupButton()
+    {
+        click(signupButton,3);
+    }
+    private void insertSignupEmail(String text)
+    {
+        sendKeys(signupEmailField,3,text);
+    }
+    public String getValidationSignup()
+    {
+        return getText(getMessageEmailExist,3);
+    }
     public HomePage loginFeature(String email,String password)
     {
         insertEmail(email);
@@ -46,5 +65,11 @@ public class LoginPage extends MethodHandles {
         clickOnLoginButton();
         return new HomePage(driver);
 
+    }
+    public void signupFeature(String signupemail,String name)
+    {
+        insertSignupEmail(signupemail);
+        insertName(name);
+        clickOnSignupButton();
     }
 }
