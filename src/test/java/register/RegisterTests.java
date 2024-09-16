af9621595776ff0fd951efec5adddd435d0e6c38
MfawzyIbrahim <mahmoud.f.s.ibrahim@gmail.com>
2024-09-16 21:22:00 +0300

    solve package register

--- src/test/java/register/RegisterTests.java
+++ src/test/java/register/RegisterTests.java
@@ -5,10 +5,13 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.DeleteAccountPage;
 import pages.LoginPage;
-
+import pages.RegisterPage;
 import java.io.FileNotFoundException;
 
 public class RegisterTests extends BaseTests {
+    LoginPage log;
+    RegisterPage register;
+
     @Test
     public void testRegisterUserWithExistingEmail() throws FileNotFoundException {
         //Verify that home page is visible successfully
@@ -23,4 +26,53 @@ public class RegisterTests extends BaseTests {
         //Verify error 'Email Address already exist!' is visible
       Assert.assertEquals(loginPage.getValidationSignup(),"Email Address already exist!");
     }
+    @Test
+    public void testRegisterUserTC1() throws FileNotFoundException {
+
+        Assert.assertTrue(homePage.homePageVisible(),"Home page is not visible");
+
+        log = homePage.clickOnSignUpAndLoginLink();
+
+
+        register = registerPage;
+        Assert.assertEquals(register.userSignUpTextVisible() , "New User Signup!");
+        register.insertRegisterName(dataModel().Registration.ValidCredentials.RegisterName);
+        register.insertRegisterEmail(dataModel().Registration.ValidCredentials.RegisterEmail);
+        register.clickOnRegisterBtn();
+
+
+        Assert.assertEquals(register.getEnterAccountEInfoText() , "ENTER ACCOUNT INFORMATION");
+
+        register.registerAccountForm(dataModel().RegistrationAccountForm.ValidRegisterAccountCredentials.RegisterAccountPassword
+                ,dataModel().RegistrationAccountForm.ValidRegisterAccountCredentials.RegisterBirthDay
+                ,dataModel().RegistrationAccountForm.ValidRegisterAccountCredentials.RegisterBirthMonth
+                ,dataModel().RegistrationAccountForm.ValidRegisterAccountCredentials.RegisterBirthYear
+                ,dataModel().RegistrationAccountForm.ValidRegisterAccountCredentials.FirstNameAddressA
+                ,dataModel().RegistrationAccountForm.ValidRegisterAccountCredentials.LastNameAddressA
+                ,dataModel().RegistrationAccountForm.ValidRegisterAccountCredentials.CompanyNameAddressA
+                ,dataModel().RegistrationAccountForm.ValidRegisterAccountCredentials.AddressNameA1
+                ,dataModel().RegistrationAccountForm.ValidRegisterAccountCredentials.AddressNameA2
+                ,dataModel().RegistrationAccountForm.ValidRegisterAccountCredentials.CountryAddressA
+                ,dataModel().RegistrationAccountForm.ValidRegisterAccountCredentials.StateA
+                ,dataModel().RegistrationAccountForm.ValidRegisterAccountCredentials.CityA
+                ,dataModel().RegistrationAccountForm.ValidRegisterAccountCredentials.ZipCodeA
+                ,dataModel().RegistrationAccountForm.ValidRegisterAccountCredentials.MobileNumberA);
+
+
+
+
+        Assert.assertEquals(register.accountCreatedTxtVisible() , "ACCOUNT CREATED!");
+        register.clickOnContinueBtn();
+
+
+        Assert.assertEquals(register.loggedInUserNameTxtVisible() , "Logged in as ibrahim");
+
+
+
+        register.clickOnDeleteAccount();
+        Assert.assertEquals(register.accountDeletedTxtVisible() , "ACCOUNT DELETED!");
+        register.clickOnContinueDeleteBtn();
+
+    }
+
 }
